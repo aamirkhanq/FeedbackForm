e5f24c57e8838ff7b8cf24e2348ab3cd7a8b36d4
Aamir Khan <aamir.k56@gmail.com>
2017-07-10 00:03:36 +0530

    Correct Typo

--- project.py
+++ project.py
@@ -18,7 +18,7 @@ def saveFeedback():
 		response3 = request.form['s1']
 		conn = psycopg2.connect("dbname=feedbacks")
 		cursor = conn.cursor()
-		cursor.execute("select feedback_no from feedbacks order by desc limit 1;")
+		cursor.execute("select feedback_no from feedbacks order by feedback_no desc limit 1;")
 		last_feedback = cursor.fetchall()
 		try:
 			feedback_no = int(last_feedback[0]) + 1
