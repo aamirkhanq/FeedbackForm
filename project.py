01e05f9c33ed58f537538bbe20314569e2971d07
Aamir Khan <aamir.k56@gmail.com>
2017-07-09 23:55:52 +0530

    Correct Typo

--- project.py
+++ project.py
@@ -22,12 +22,12 @@ def saveFeedback():
 		last_feedback = cursor.fetchall()
 		try:
 			feedback_no = int(last_feedback[0]) + 1
-    		cursor.execute("insert into feedbacks values (%s, %s, %s, %s);" %(feedback_no, response1, response2, response3))
-    	except:
-    		feedback_no = 1
-    		cursor.execute("insert into feedbacks values (%s, %s, %s, %s);" %(feedback_no, response1, response2, response3))
-    	conn.commit()
-    	conn.close()
+			cursor.execute("insert into feedbacks values (%s, %s, %s, %s);" %(feedback_no, response1, response2, response3))
+		except:
+			feedback_no = 1
+			cursor.execute("insert into feedbacks values (%s, %s, %s, %s);" %(feedback_no, response1, response2, response3))
+			conn.commit()
+			conn.close()
 
 	return redirect(url_for('thankYouPage'))
 
