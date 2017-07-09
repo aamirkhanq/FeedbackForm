84b81a4b85a46473c8de8e4ccaed4fdcea319b1e
Aamir Khan <aamir.k56@gmail.com>
2017-07-09 23:09:09 +0530

    Add function to record responses

--- project.py
+++ project.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def MainPage():
 	return render_template('index.html')
 
+@app.route('/savefeedback', methods=['GET', 'POST'])
+def saveFeedback():
+	if request.method == "POST":
+		response1 = request.form['q1']
+		response2 = request.form['r1']
+		response3 = request.form['s1']
+		print response1, response2, response3
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0', port=5000)
