f227a7ffd8079ef05bad14887f6e60788ae5abfa
aamirkhanq <aamir.k56@gmail.com>
2017-07-18 23:25:50 -0700

    Final Touch: Add graph using pygal module

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import psycopg2
+import psycopg2, pygal
 
 app = Flask(__name__)
 
@@ -38,34 +38,59 @@ def thankYouPage():
 	conn = psycopg2.connect("dbname=feedbacks")
 	cursor = conn.cursor()
 	cursor.execute("select count(*) as count from feedbacks;")
-	total_feedbacks = cursor.fetchall()[0][0]
+	total_feedbacks = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response1='Yes'")
-	total_yes_option1 = cursor.fetchall()[0][0]
+	total_yes_option1 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response1='No'")
-	total_no_option1 = cursor.fetchall()[0][0]
+	total_no_option1 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response1='Cannot Say'")
-	total_cant_option1 = cursor.fetchall()[0][0]
+	total_cant_option1 = float(cursor.fetchall()[0][0])
 
 	cursor.execute("select count(*) from feedbacks where response2='Yes'")
-	total_yes_option2 = cursor.fetchall()[0][0]
+	total_yes_option2 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response2='No'")
-	total_no_option2 = cursor.fetchall()[0][0]
+	total_no_option2 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response2='Cannot Say'")
-	total_cant_option2 = cursor.fetchall()[0][0]
+	total_cant_option2 = float(cursor.fetchall()[0][0])
 
 	cursor.execute("select count(*) from feedbacks where response3='Yes'")
-	total_yes_option3 = cursor.fetchall()[0][0]
+	total_yes_option3 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response3='No'")
-	total_no_option3 = cursor.fetchall()[0][0]
+	total_no_option3 = float(cursor.fetchall()[0][0])
 	cursor.execute("select count(*) from feedbacks where response3='Cannot Say'")
-	total_cant_option3 = cursor.fetchall()[0][0]
+	total_cant_option3 = float(cursor.fetchall()[0][0])
+        
+        data1 = [(total_yes_option1/total_feedbacks)*100, (total_no_option1/total_feedbacks)*100, (total_cant_option1/total_feedbacks)*100]
+        data2 = [(total_yes_option2/total_feedbacks)*100, (total_no_option2/total_feedbacks)*100, (total_cant_option2/total_feedbacks)*100]
+        data3 = [(total_yes_option3/total_feedbacks)*100, (total_no_option3/total_feedbacks)*100, (total_cant_option3/total_feedbacks)*100]
+
+        options = ["Yes", "No", "Can't Say"]
+
+        bar_chart1 = pygal.Bar(width=600, height=300,
+                          explicit_size=True, title="Q1.",
+                          disable_xml_declaration=True)
+
+        bar_chart1.x_labels = options
+
+        bar_chart2 = pygal.Bar(width=600, height=300,
+                          explicit_size=True, title="Q2.",
+                          disable_xml_declaration=True)
+
+        bar_chart2.x_labels = options
+
+        bar_chart3 = pygal.Bar(width=600, height=300,
+                          explicit_size=True, title="Q3.",
+                          disable_xml_declaration=True)
+
+        bar_chart3.x_labels = options
+        
+        bar_chart1.add('Answers for Q1.', data1)
+        bar_chart2.add('Answers for Q2.', data2)
+        bar_chart3.add('Answers for Q3.', data3)
 
 	conn.close()
 
-	return render_template('thankyou.html', total_feedbacks=total_feedbacks, total_yes_option1=total_yes_option1, 
-		total_no_option1=total_no_option1, total_cant_option1=total_cant_option1, total_yes_option2=total_yes_option2,
-		total_no_option2=total_no_option2, total_cant_option2=total_cant_option2, total_yes_option3=total_yes_option3,
-		total_no_option3=total_no_option3, total_cant_option3=total_cant_option3)
+	return render_template('thankyou.html', bar_chart1 = bar_chart1, bar_chart2 = bar_chart2, bar_chart3 = bar_chart3)
 
 if __name__ == '__main__':
 	app.debug = True
