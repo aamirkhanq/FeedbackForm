5b1b51092798833a2f4654d155ea037ec8ccb67e
Aamir Khan <aamir.k56@gmail.com>
2017-08-07 01:50:22 +0530

    Change question

--- feedbacks.sql
+++ feedbacks.sql
@@ -1,9 +1,11 @@
 DROP DATABASE IF EXISTS feedbacks;
 create database feedbacks;
 \c feedbacks;
+DROP TABLE IF EXISTS feedbacks;
 create table feedbacks(
 feedback_no serial primary key,
 response1 text,
 response2 text,
-response3 text
+response3 text,
+response4 text
 );
--- project.py
+++ project.py
@@ -11,18 +11,20 @@ def MainPage():
 		response1 = request.form['q1']
 		response2 = request.form['r1']
 		response3 = request.form['s1']
+                response4 = request.form['t1']
+
 		conn = psycopg2.connect("dbname=feedbacks")
 		cursor = conn.cursor()
 		cursor.execute("select feedback_no from feedbacks order by feedback_no desc limit 1;")
 		last_feedback = cursor.fetchall()
 		try:
 			feedback_no = int(last_feedback[0][0]) + 1
-			cursor.execute('insert into feedbacks ("feedback_no", "response1", "response2", "response3") values ('+ "'%s', '%s', '%s', '%s');" %(feedback_no, response1, response2, response3))
+			cursor.execute('insert into feedbacks ("feedback_no", "response1", "response2", "response3", "response4") values ('+ "'%s', '%s', '%s', '%s', '%s');" %(feedback_no, response1, response2, response3, response4))
 			conn.commit()
 			conn.close()
 		except:
 			feedback_no = 1
-			cursor.execute('insert into feedbacks ("feedback_no", "response1", "response2", "response3") values ('+ "'%s', '%s', '%s', '%s');" %(feedback_no, response1, response2, response3))
+			cursor.execute('insert into feedbacks ("feedback_no", "response1", "response2", "response3", "response4") values ('+ "'%s', '%s', '%s', '%s', '%s');" %(feedback_no, response1, response2, response3, response4))
 			conn.commit()
 			conn.close()
 		return redirect(url_for('saveFeedback'))
@@ -39,32 +41,50 @@ def thankYouPage():
 	cursor = conn.cursor()
 	cursor.execute("select count(*) as count from feedbacks;")
 	total_feedbacks = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response1='Yes'")
-	total_yes_option1 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response1='No'")
-	total_no_option1 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response1='Cannot Say'")
-	total_cant_option1 = float(cursor.fetchall()[0][0])
+	if total_feedbacks == 0:
+                total_feedbacks = 1;
+	cursor.execute("select count(*) from feedbacks where response1='Strongly Disagree'")
+	total_1_option1 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Disagree'")
+	total_2_option1 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Agree'")
+	total_3_option1 = float(cursor.fetchall()[0][0])
+        cursor.execute("select count(*) from feedbacks where response1='Strongly Agree'")
+	total_4_option1 = float(cursor.fetchall()[0][0])
 
-	cursor.execute("select count(*) from feedbacks where response2='Yes'")
-	total_yes_option2 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response2='No'")
-	total_no_option2 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response2='Cannot Say'")
-	total_cant_option2 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Strongly Disagree'")
+	total_1_option2 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Disagree'")
+	total_2_option2 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Agree'")
+	total_3_option2 = float(cursor.fetchall()[0][0])
+        cursor.execute("select count(*) from feedbacks where response1='Strongly Agree'")
+	total_4_option2 = float(cursor.fetchall()[0][0])
 
-	cursor.execute("select count(*) from feedbacks where response3='Yes'")
-	total_yes_option3 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response3='No'")
-	total_no_option3 = float(cursor.fetchall()[0][0])
-	cursor.execute("select count(*) from feedbacks where response3='Cannot Say'")
-	total_cant_option3 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Strongly Disagree'")
+	total_1_option3 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Disagree'")
+	total_2_option3 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Agree'")
+	total_3_option3 = float(cursor.fetchall()[0][0])
+        cursor.execute("select count(*) from feedbacks where response1='Strongly Agree'")
+	total_4_option3 = float(cursor.fetchall()[0][0])
+
+        cursor.execute("select count(*) from feedbacks where response1='Strongly Disagree'")
+	total_1_option4 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Disagree'")
+	total_2_option4 = float(cursor.fetchall()[0][0])
+	cursor.execute("select count(*) from feedbacks where response1='Agree'")
+	total_3_option4 = float(cursor.fetchall()[0][0])
+        cursor.execute("select count(*) from feedbacks where response1='Strongly Agree'")
+	total_4_option4 = float(cursor.fetchall()[0][0])
         
-        data1 = [(total_yes_option1/total_feedbacks)*100, (total_no_option1/total_feedbacks)*100, (total_cant_option1/total_feedbacks)*100]
-        data2 = [(total_yes_option2/total_feedbacks)*100, (total_no_option2/total_feedbacks)*100, (total_cant_option2/total_feedbacks)*100]
-        data3 = [(total_yes_option3/total_feedbacks)*100, (total_no_option3/total_feedbacks)*100, (total_cant_option3/total_feedbacks)*100]
+        data1 = [(total_1_option1/total_feedbacks)*100, (total_2_option1/total_feedbacks)*100, (total_3_option1/total_feedbacks)*100, (total_4_option1/total_feedbacks)*100]
+        data2 = [(total_1_option2/total_feedbacks)*100, (total_2_option2/total_feedbacks)*100, (total_3_option2/total_feedbacks)*100, (total_4_option2/total_feedbacks)*100]
+        data3 = [(total_1_option3/total_feedbacks)*100, (total_2_option3/total_feedbacks)*100, (total_3_option3/total_feedbacks)*100, (total_4_option3/total_feedbacks)*100]
+        data4 = [(total_1_option4/total_feedbacks)*100, (total_2_option4/total_feedbacks)*100, (total_3_option4/total_feedbacks)*100, (total_4_option4/total_feedbacks)*100]
 
-        options = ["Yes", "No", "Can't Say"]
+        options = ["Strongly Disagree", "Disagree", "Agree", "Strongly Agree"]
 
         bar_chart1 = pygal.Bar(width=600, height=300,
                           explicit_size=True, title="Q1.",
@@ -83,14 +103,21 @@ def thankYouPage():
                           disable_xml_declaration=True)
 
         bar_chart3.x_labels = options
+
+        bar_chart4 = pygal.Bar(width=600, height=300,
+                          explicit_size=True, title="Q3.",
+                          disable_xml_declaration=True)
+
+        bar_chart4.x_labels = options
         
         bar_chart1.add('Answers for Q1.', data1)
         bar_chart2.add('Answers for Q2.', data2)
         bar_chart3.add('Answers for Q3.', data3)
+        bar_chart4.add('Answers for Q4.', data4)
 
 	conn.close()
 
-	return render_template('thankyou.html', bar_chart1 = bar_chart1, bar_chart2 = bar_chart2, bar_chart3 = bar_chart3)
+	return render_template('thankyou.html', bar_chart1 = bar_chart1, bar_chart2 = bar_chart2, bar_chart3 = bar_chart3, bar_chart4 = bar_chart4)
 
 if __name__ == '__main__':
 	app.debug = True
